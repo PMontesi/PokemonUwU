002d1884e453c3c996f820bfdb5128ae2f243bfc
PMontesi <montesinos.nicolas96@gmail.com>
2024-04-11 20:05:20 +0200

    Controladores de las pantallas captura, equipo y loginh

---
+++ src/main/java/es/cesur/progprojectpok/controllers/CapturaController.java
@@ -0,0 +1,14 @@
+package es.cesur.progprojectpok.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CapturaController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
