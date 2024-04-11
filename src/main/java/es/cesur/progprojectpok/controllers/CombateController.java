1dd44764953fca202bbad2f63f4bd67c8cbcc943
PMontesi <montesinos.nicolas96@gmail.com>
2024-04-11 19:57:05 +0200

    commit de prueba de la rama de pruebas

---
+++ src/main/java/es/cesur/progprojectpok/controllers/CombateController.java
@@ -0,0 +1,16 @@
+package es.cesur.progprojectpok.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CombateController implements Initializable {
+
+    int patat;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
