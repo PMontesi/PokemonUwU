58750c0e23dfb314c7674d82a0a15edeef442c9d
PMontesi <montesinos.nicolas96@gmail.com>
2024-04-19 23:55:00 +0200

    Lógica de la vista Captura Se genera un pokemon para capturar. Falta la imagen.

--- src/main/java/es/cesur/progprojectpok/clases/Pokemon.java
+++ src/main/java/es/cesur/progprojectpok/clases/Pokemon.java
@@ -2,23 +2,244 @@ package es.cesur.progprojectpok.clases;
 
 public class Pokemon {
 
-    String nombre;
-    String mote;
-    int vitalidad;
-    int ataque;
-    int defensa;
-    int ataqueEspecial;
-    int defensaEspecial;
-    int velocidad;
-    int nivel;
-    char sexo;
-    Tipos tipo1;
-    Tipos tipo2;
-    EstadoPersistente estadoPersistente;
-    EstadoTemporal estadotemporal;
+    private String nombre;
+    private String mote;
+    private int vitalidad;
+    private int ataque;
+    private int defensa;
+    private int ataqueEspecial;
+    private int defensaEspecial;
+    private int velocidad;
+    private int nivel;
+    private int experiencia;
+    private char sexo;
+    private int fertilidad;
+    private static final int VALOR_INICIAL_FERT = 5;
+    private Tipos tipo1;
+    private Tipos tipo2;
+    private static Movimiento[] movimientos = new Movimiento[4];
+    private EstadoPersistente estadoPersistente;
+    private EstadoTemporal estadotemporal;
+    private Objeto objetoEquipado;
+
+    public Pokemon() {
+    }
+
+    //Constructor de pokemons que nacen. Principalmente para la pantalla de captura.
+    public Pokemon(String nombre, Tipos tipo1, Tipos tipo2) {
+        this.nombre = nombre;
+        this.vitalidad = (int) (Math.random()*10 + 1);
+        this.ataque = (int) (Math.random()*10 + 1);
+        this.defensa = (int) (Math.random()*10 + 1);
+        this.ataqueEspecial = (int) (Math.random()*10 + 1);
+        this.defensaEspecial = (int) (Math.random()*10 + 1);
+        this.velocidad = (int) (Math.random()*10 + 1);
+        this.nivel = 1;
+        this.experiencia = 0;
+        this.sexo = randomSex();
+        this.fertilidad = VALOR_INICIAL_FERT;
+        this.tipo1 = tipo1;
+        this.tipo2 = tipo2;
+    }
+
+    public char randomSex(){
+        char sexo;
+        int sexoBinario = (int) (Math.random()*2);
+        if (sexoBinario == 0) sexo = 'H';
+        else sexo = 'M';
+        return sexo;
+    }
+
+    public static Tipos TipoStringToEnum(String tipoString){
+        return switch (tipoString.toUpperCase()) {
+            case "AGUA" -> Tipos.AGUA;
+            case "BICHO" -> Tipos.BICHO;
+            case "DRAGÓN" -> Tipos.DRAGON;
+            case "ELÉCTRICO" -> Tipos.ELECTRICO;
+            case "FANTAMAS" -> Tipos.FANTASMA;
+            case "FUEGO" -> Tipos.FUEGO;
+            case "HIELO" -> Tipos.HIELO;
+            case "LUCHA" -> Tipos.LUCHA;
+            case "NORMAL" -> Tipos.NORMAL;
+            case "PLANTA" -> Tipos.PLANTA;
+            case "PSÍQUICO" -> Tipos.PSIQUICO;
+            case "ROCA" -> Tipos.ROCA;
+            case "TIERRA" -> Tipos.TIERRA;
+            case "VENENO" -> Tipos.VENENO;
+            case "VOLADOR" -> Tipos.VOLADOR;
+            case "NULL" -> Tipos.NULL;
+            default -> null;
+        };
+    }
 
 
+    //GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getMote() {
+        return mote;
+    }
 
+    public void setMote(String mote) {
+        this.mote = mote;
+    }
+
+    public int getVitalidad() {
+        return vitalidad;
+    }
+
+    public void setVitalidad(int vitalidad) {
+        this.vitalidad = vitalidad;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getAtaqueEspecial() {
+        return ataqueEspecial;
+    }
+
+    public void setAtaqueEspecial(int ataqueEspecial) {
+        this.ataqueEspecial = ataqueEspecial;
+    }
+
+    public int getDefensaEspecial() {
+        return defensaEspecial;
+    }
+
+    public void setDefensaEspecial(int defensaEspecial) {
+        this.defensaEspecial = defensaEspecial;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getFertilidad() {
+        return fertilidad;
+    }
+
+    public void setFertilidad(int fertilidad) {
+        this.fertilidad = fertilidad;
+    }
+
+    public Tipos getTipo1() {
+        return tipo1;
+    }
+
+    public void setTipo1(Tipos tipo1) {
+        this.tipo1 = tipo1;
+    }
+
+    public Tipos getTipo2() {
+        return tipo2;
+    }
+
+    public void setTipo2(Tipos tipo2) {
+        this.tipo2 = tipo2;
+    }
+
+    public Movimiento[] getMovimientos() {
+        return movimientos;
+    }
+
+    public void setMovimientos(Movimiento[] movimientos) {
+        this.movimientos = movimientos;
+    }
+
+    public EstadoPersistente getEstadoPersistente() {
+        return estadoPersistente;
+    }
+
+    public void setEstadoPersistente(EstadoPersistente estadoPersistente) {
+        this.estadoPersistente = estadoPersistente;
+    }
+
+    public EstadoTemporal getEstadotemporal() {
+        return estadotemporal;
+    }
+
+    public void setEstadotemporal(EstadoTemporal estadotemporal) {
+        this.estadotemporal = estadotemporal;
+    }
+
+    public Objeto getObjetoEquipado() {
+        return objetoEquipado;
+    }
+
+    public void setObjetoEquipado(Objeto objetoEquipado) {
+        this.objetoEquipado = objetoEquipado;
+    }
+
+    //TO STRING
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "nombre='" + nombre + '\'' +
+                ", mote='" + mote + '\'' +
+                ", vitalidad=" + vitalidad +
+                ", ataque=" + ataque +
+                ", defensa=" + defensa +
+                ", ataqueEspecial=" + ataqueEspecial +
+                ", defensaEspecial=" + defensaEspecial +
+                ", velocidad=" + velocidad +
+                ", nivel=" + nivel +
+                ", experiencia=" + experiencia +
+                ", sexo=" + sexo +
+                ", fertilidad=" + fertilidad +
+                ", tipo1=" + tipo1 +
+                ", tipo2=" + tipo2 +
+                ", estadoPersistente=" + estadoPersistente +
+                ", estadotemporal=" + estadotemporal +
+                ", objetoEquipado=" + objetoEquipado +
+                '}';
+    }
 }
--- src/main/java/es/cesur/progprojectpok/controllers/CapturaController.java
+++ src/main/java/es/cesur/progprojectpok/controllers/CapturaController.java
@@ -1,14 +1,122 @@
 package es.cesur.progprojectpok.controllers;
 
+import es.cesur.progprojectpok.SplashApplication;
+import es.cesur.progprojectpok.clases.Pokemon;
+import es.cesur.progprojectpok.clases.Tipos;
+import es.cesur.progprojectpok.database.DBConnection;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class CapturaController implements Initializable {
 
+
+    @FXML
+    private Button botonCapturar;
+    @FXML
+    private Button botonMenuPrincipal;
+    @FXML
+    private ImageView pokemonSalvaje;
+    @FXML
+    private ImageView imagenEntrenador;
+    @FXML
+    private TextArea logCaptura;
+
+    public void volverMenu() {
+        Stage stage = (Stage) botonMenuPrincipal.getScene().getWindow();
+        stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(SplashApplication.class.getResource("view/mainMenu-view.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 800, 480);
+            stage.setTitle("Menu");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        int numPokedex = 0;
+
+        String sqlSelectTotalPokedex = "SELECT MAX(NUM_POKEDEX) FROM POKEDEX";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlSelectTotalPokedex)) {
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                numPokedex = resultSet.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        int pokemonAleatorio = (int) (Math.random() * numPokedex + 1);
+        System.out.println(pokemonAleatorio);
+
+        String sqlSelectPokemon = "SELECT * FROM POKEDEX WHERE NUM_POKEDEX = ?";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement statementSelectPokemon = connection.prepareStatement(sqlSelectPokemon)) {
+            statementSelectPokemon.setInt(1, pokemonAleatorio);
+            ResultSet resultSetPokemon = statementSelectPokemon.executeQuery();
+            while (resultSetPokemon.next()) {
+                String nombre = resultSetPokemon.getString("NOM_POKEMON");
+                String tipo1 = resultSetPokemon.getString("TIPO1");
+                String tipo2 = resultSetPokemon.getString("TIPO2");
+                System.out.println(tipo2);
+                if (tipo2 == null){ tipo2 = "null";};
+                Pokemon pokemon = new Pokemon(nombre, Pokemon.TipoStringToEnum(tipo1), Pokemon.TipoStringToEnum(tipo2));
+                String imagenMacho = resultSetPokemon.getString("IMAGEN_DELANTE");
+                String imagenHembra= resultSetPokemon.getString("IMAGEN_DELANTE_F");
+
+                System.out.println(pokemon.toString());
+                System.out.println(imagenMacho);
+                System.out.println(imagenHembra);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+
+        }
+
+
+        //SUSTITUCIÓN DE IMÁGEN. NO FUNCIONA.
+        /*
+        URL imageUrl = getClass().getResource("images/pokemons/arbok-front");
+        System.out.println(imageUrl);
+        Image imagenDeRelleno = new Image(imageUrl.toString());
+
+        pokemonSalvaje.setImage(imagenDeRelleno);
+
+        if (imagen != null && !imagen.isEmpty()) {
+            Image imagenPokemonSalvaje = new Image(imagen);
+            pokemonSalvaje.setImage(imagenPokemonSalvaje);
+        } else {
+            // Si la URL de la imagen es nula o vacía, puedes mostrar una imagen de relleno o dejar el ImageView vacío
+            // Por ejemplo, para mostrar una imagen de relleno:
+            Image imagenDeRelleno = new Image("/images/entren-espaldas.png");
+            pokemonSalvaje.setImage(imagenDeRelleno);
+        }
+
+         */
+
 
     }
 }
