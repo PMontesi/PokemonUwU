ac1392b1a4f00a6c3c6f2df107bbd2e45a6fc108
PMontesi <montesinos.nicolas96@gmail.com>
2024-04-25 14:59:18 +0200

    Clases combate tunro (no actualizadas)

---
+++ src/main/java/es/cesur/progprojectpok/clases/Combate.java
@@ -0,0 +1,18 @@
+package es.cesur.progprojectpok.clases;
+
+public class Combate {
+
+    Turno turno;
+    Entrenador jugador;
+    Entrenador rival;
+    int koJugador;
+    int koRival;
+    Entrenador ganador = null;
+
+
+    public void combatir(Entrenador jugador, Entrenador rival){}
+    public void retirarse(Entrenador jugador){}
+    public void determinarGanador(){}
+    public void entregarPokedolares(){}
+    public void recibirExperiencia(){}
+}
---
+++ src/main/java/es/cesur/progprojectpok/clases/Turno.java
@@ -0,0 +1,14 @@
+package es.cesur.progprojectpok.clases;
+
+public class Turno {
+
+    private int numeroCombate;
+    private int numeroTurno;
+    private String accionJugador;
+    private String accionRival;
+
+
+    public void crearLog(){
+
+    }
+}
